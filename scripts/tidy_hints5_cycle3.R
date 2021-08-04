e90626d2ce54fb2e1e6e3c6b1c260045bd677205
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-04 11:28:18 -0700

    new files

---
+++ scripts/tidy_hints5_cycle3.R
@@ -0,0 +1,37 @@
+# What does the NCI HINTS 5, Cycle 3 dataset tell us?
+# René Dario Herrera
+# University of Arizona Cancer Center 
+# renedherrera at email dot arizona dot edu 
+
+# set up ####
+# packages 
+library(here)
+library(tidyverse)
+library(haven)
+library(janitor)
+
+# prepare to read data 
+# set values
+url <- "https://hints.cancer.gov/dataset/HINTS5_Cycle3_SAS_20210305.zip"
+path_zip <- "data/raw"
+path_unzip <- "data/raw/HINTS5_Cycle3_SAS_20210305"
+zip_file <- "HINTS5_Cycle3_SAS_20210305.zip"
+# use curl to download
+curl::curl_download(url, destfile = paste(path_zip, zip_file, sep = "/"))
+# set value
+zipped_file <- "data/raw/HINTS5_Cycle3_SAS_20210305.zip"
+# unzip to folder
+unzip(zipped_file, exdir = path_unzip)
+
+# read data from file 
+hints5_c3 <- read_sas(
+  data_file = "data/raw/HINTS5_Cycle3_SAS_20210305/hints5_cycle3_public.sas7bdat"
+) %>%
+  clean_names()
+
+# inspect 
+glimpse(hints5_c3)
+
+
+
+
